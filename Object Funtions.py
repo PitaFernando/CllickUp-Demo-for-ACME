a33a5eb5d43b0ea0a1106b4ae1e37099fcc34ab7
Fernando Pita <fpitaf@gmail.com>
2025-08-28 14:04:03 -0400

    Add files via upload

---
+++ Object Funtions.py
@@ -0,0 +1,8 @@
+
+from student1 import Student
+
+student1 = Student("Jose", "Arquitectura", 3.6)
+student2 = Student("Juan", "Matematicas", 2.8)
+
+print(student1.on_honr_roll())
+print(student2.on_honr_roll())

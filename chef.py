4dd12f85e215c302c5912db39690d5486e33f54c
Fernando Pita <fpitaf@gmail.com>
2025-08-30 15:55:18 -0400

    Add files via upload

---
+++ chef.py
@@ -0,0 +1,13 @@
+
+
+class Chef:
+
+    def make_chicken(self):
+        print ("The Chef makes chicken")
+
+    def make_salad(self):
+        print("The Chef makes salas")
+
+    def make_special_dish(self):
+        print("The Chef Makes special dish")
+
